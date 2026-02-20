a42c66ec496ef94bd0775b07c4c87b0dc2226e58
Ashley Alvarado <ashleyalvarado@Mac.lan>
2026-02-19 16:10:16 -0800

    initial commit

---
+++ Main.py
@@ -0,0 +1,21 @@
+import pygame
+import os
+
+pygame.init()
+
+pygame.display.set_caption("Escape the Basement")
+screen = pygame.display.set_mode((1000, 700))
+menuTitle = pygame.image.load('EtBTitle.png')
+
+running = True
+while running:
+    pygame.display.flip()
+    screen.fill((34, 25, 14))
+    pygame.time.delay(150)
+    screen.blit(menuTitle, (25, -170))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+
+pygame.quit()
