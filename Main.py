0a112b86826df05125e946a919dde207d77a3ead
Matthew Benitez <2ohyeahMB200@csu.fullerton.edu>
2026-03-08 13:06:59 -0700

    updated the main.py

--- Main.py
+++ Main.py
@@ -1,47 +1,28 @@
 import pygame
 import os
+from Button import Button
 
 pygame.init()
 
 pygame.display.set_caption("Escape the Basement")
-screen = pygame.display.set_mode((1000, 700)) # 1466, 825
+screen = pygame.display.set_mode((1000, 700))
 menuTitle = pygame.image.load('EtBTitle.png')
 menuBackground = pygame.image.load('basement-2.v1.jpg')
 
-Button_Color = (100, 100, 100)
-Button_hover_color = (150, 150, 150)
+Button_Color = ('red')
+Button_hover_color = ('blue')
 font = pygame.font.Font(None, 40)
-
-class Button:
-    def __init__(self, color, x, y, width, height, text='', action = None):
-        self.color = color
-        self.rect = pygame.Rect(x, y, width, height)
-        self.text = text
-        self.action = action
-
-    def draw(self, screen, outline=None):
-        pygame.draw.rect(screen, self.color, self.rect, 0)
-        
-        if self.text != '':
-            text_surface = font.render(self.text, True, (255, 255, 255))
-            text_rect = text_surface.get_rect(center=self.rect.center)
-            screen.blit(text_surface, text_rect)
-
-    def is_clicked(self, pos):
-        return self.rect.collidepoint(pos)
-
 play_button = Button(Button_Color, 300, 400, 200, 100, 'Play', None)
 quit_button = Button(Button_Color, 500, 400, 200, 100, 'Quit', None)
 
-
-def displayMenu(): # please do not fill the screen on every frame.
+def displayMenu():
     pygame.display.flip()
     screen.fill((34, 25, 14))
     screen.blit(menuBackground,(0,0))
     screen.blit(menuTitle, (25, -170))
 
-    play_button.draw(screen)
-    quit_button.draw(screen)
+    play_button.draw(screen, font)
+    quit_button.draw(screen, font)
     pygame.display.flip()
 
 running = True
@@ -73,10 +54,5 @@ while running:
 
     play_button.color = current_button_color
     quit_button.color = current_quit_button_color
-    play_button.draw(screen)
-    quit_button.draw(screen)
-    #pygame.display.flip()
-            
-    
 
 pygame.quit()
