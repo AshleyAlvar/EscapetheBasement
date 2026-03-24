b2ea5bf84d93d822b65e3fc86988fbd724b79c0d
Matthew Benitez <2ohyeahMB200@csu.fullerton.edu>
2026-03-24 16:19:45 -0700

    Add Intro separetly

--- IntroScene.py
+++ IntroScene.py
@@ -1,12 +1,21 @@
 import pygame
-pygame.init()
-screen = pygame.display.set_mode((1000, 700))
-font = pygame.font.Font(None, 40)
+from GameScene import GameScene
 
-def draw_intro():
-    screen.fill('black')
-    intro_text = font.render("You are trapped in a dark basement. Ready to escape?", True, (255, 255, 255))
-    intro_rect = intro_text.get_rect(center=(640, 360))
-    screen.blit(intro_text, intro_rect)
-    pygame.display.flip()
+class IntroScene:
+    def __init__(self, screen):
+        self.screen = screen
+        self.font = pygame.font.Font(None, 40)
+        self.text = "You are trapped in a dark basement. Ready to escape?"
+
+    def handle_events(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                return GameScene(self.screen)
+        return None
+    
+    def draw_intro(self):
+        self.screen.fill('black')
+        intro_text = self.font.render(self.text, True, (255, 255, 255))
+        intro_rect = intro_text.get_rect(center=(640, 360))
+        self.screen.blit(intro_text, intro_rect)
 
